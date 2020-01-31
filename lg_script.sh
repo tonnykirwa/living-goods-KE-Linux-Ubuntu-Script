3ce0e5228e9fb5ce6bbb2503078a9593ff850403
tonnykirwa <tonnykirwa@users.noreply.github.com>
2020-01-31 15:31:49 +0300

    Add files via upload

---
+++ lg_script.sh
@@ -0,0 +1,11 @@
+sudo adb start-server
+
+adb devices
+
+adb install -r lg_v3.apk
+	echo success installed lg live app
+adb install -r training_lg.apk
+	echo success installed training lg
+adb install -r go_launcher.apk
+	echo success installed launcher
+
